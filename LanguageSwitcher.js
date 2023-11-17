b785fc5d5fd73a724244107a9a57542cbeb55afa
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-11-17 16:54:52 +0100

    Language Selector Updating

--- LanguageSwitcher.js
+++ LanguageSwitcher.js
@@ -1,15 +1,21 @@
 document.getElementById('languageSelector').addEventListener('change', function () {
-  var language = this.value;
-  switch (language) {
-    case 'en':
-        window.location.href = 'https://en.einfachwebsiteerstellen.de/'; // 设置为根目录下的 en 文件夹中的 index_en.html
-        break;
-    case 'de':
-        window.location.href = 'https://einfachwebsiteerstellen.de/'; // 替换为德语页面的实际 URL
-        break;
-    case 'cn':
-        window.location.href = 'https://cn.einfachwebsiteerstellen.de/'; // 替换为中文页面的实际 URL
-        break;
-    // 可以根据需要添加更多的 case
-}
+    var language = this.value;
+    var currentUrl = new URL(window.location.href);
+    var currentPath = currentUrl.pathname; // 获取当前页面的路径
+
+    // 对于德语，我们需要移除子域名；对于其他语言，我们需要添加相应的子域名
+    var hostname = currentUrl.hostname;
+    var domain = 'einfachwebsiteerstellen.de';
+    var newSubdomain = language === 'de' ? '' : language + '.';
+    
+    // 如果当前已经是子域名，先去掉子域名部分
+    if (hostname.startsWith('en.') || hostname.startsWith('cn.')) {
+        hostname = hostname.substring(hostname.indexOf('.') + 1);
+    }
+
+    // 构建新的URL
+    var newUrl = `https://${newSubdomain}${hostname}${currentPath}`;
+
+    // 跳转到新的URL
+    window.location.href = newUrl;
 });
